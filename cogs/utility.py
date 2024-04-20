1635e83080fc69feeada676324b2c77184309c0a
ibnaleem <ibnaleem@outlook.com>
2024-04-21 00:24:14 +0100

    create joined_at command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,3 +1,5 @@
+import discord, datetime
+from typing import Optional
 from discord.ext import commands
 from discord import app_commands, Embed, Interaction
 
@@ -6,7 +8,21 @@ class Utility(commands.Cog):
         super().__init__()
         self.client = client
 
-    @app_commands.command(name="members", description="Displays the amount of members in the guild")
+    def format_dt(self, dt: datetime.datetime) -> str:
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+        day_name = days[dt.weekday()]
+        day = dt.day
+        month_name = months[dt.month - 1] 
+        year = dt.year
+        hour = dt.hour
+        minute = dt.minute
+    
+        return f"{day_name} {day} {month_name} {year} @ {hour:02}:{minute:02}"
+
+
+    @app_commands.command(name="members", description="displays the amount of members in this guild")
     async def members(self, interaction: Interaction):
 
         embed = Embed(color=0x0C0C0D)
@@ -15,6 +31,19 @@ class Utility(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
+    @app_commands.command(name="joined-at", description="displays the date the member joined this guild")
+    @app_commands.describe(member="the member to display the join date for")
+    async def joined_at(self, interaction: Interaction, member: Optional[discord.Member]=None):
+
+        if not member:
+            member = interaction.user
+
+        date = self.format_dt(member.joined_at)
+
+        embed = Embed(description=f"> joined at {date.lower()}",color=0x0C0C0D)
+        embed.set_author(name=f"{member.display_name}", icon_url=f"{member.avatar}")
+        embed.set_footer(text=f"requested by {interaction.user.name}", icon_url=f"{interaction.user.avatar}")
+        await interaction.response.send_message(embed=embed)
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Utility(client))
